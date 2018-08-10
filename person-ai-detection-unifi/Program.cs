27d8af6095a9a0a37baa3e6846603cb4af9a5646
vityabool <vtsykunov@hotmail.com>
2018-08-10 13:15:43 +0300

    Add project files.

---
+++ person-ai-detection-unifi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace person_ai_detection_unifi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
